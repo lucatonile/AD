3fd7ffacf6d6cf0a2f322dba9ea368279012c4be
lucatonile <jaamongo@gmail.com>
2017-11-01 21:45:35 +0100

    2 klarish

--- AD1/2/2.py
+++ AD1/2/2.py
@@ -16,6 +16,16 @@ def integer_sort(A, k):
     Example: integer_sort([5, 3, 6, 7, 12, 3, 6, 1, 4, 7]), 12) =
                  [1, 3, 3, 4, 5, 6, 6, 7, 7, 12]
     '''
+    Y = [0]*(k+1)
+
+    for i in range(0, len(A)):
+        Y[A[i]] = Y[A[i]]+1
+    A = []
+    for x in range(0, k+1):
+        if Y[x] > 0:
+            for i in xrange(Y[x]):
+                A.append(x)
+    return(A)
 
 class IntegerSortTest(unittest.TestCase):
     """Test Suite for integer sort problem
