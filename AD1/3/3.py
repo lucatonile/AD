d14a025bae6b3b6842b850efd5bcd28c8a5355db
lucatonile <jaamongo@gmail.com>
2017-11-08 19:45:13 +0100

    3 klarish

--- AD1/3/3.py
+++ AD1/3/3.py
@@ -3,53 +3,82 @@
 '''
 Assignment 1: Binary Multiplication
 
-Team Number: 
-Student Names: 
+Team Number:
+Student Names:
 '''
 import unittest
 
+#A and B must be lists containing elements within set S={0,1}
+def binary_add(A,B):
+    A = ''.join(str(e) for e in A)
+    B = ''.join(str(e) for e in B)
+    bin_sum = bin(int(A,2)+int(B,2))
+
+    result = list(bin_sum)
+    result.remove('b')
+    result = map(int,result)
+    return(result)
+
+
+def binary_shift(A, amount):
+    tmp = []
+    for i in range(amount):
+        A.append(0)
+    return (A)
 
 def binary_mult(A,B):
     """
     Sig:    int[0..n-1], int[0..n-1] ==> int[0..2*n-1]
-    Pre:    
-    Post:   
-    Var:    
+    Pre:
+    Post:
+    Var:
     Example:    binary_mult([0,1,1],[1,0,0]) = [0,0,1,1,0,0]
     """
-    
-    if len(A)==1 && len(B)==1:
-        return A[0]*B[0]
+    n = len(A)
 
-    if len(A)%2 != 0:
+    if len(A)==1 and len(B)==1:
+        return ([A[0]*B[0]])
+
+    #Assumes len(A)==len(B)
+    if n%2 != 0:
         A.insert(0,0)
-    if len(B)%2 != 0:
         B.insert(0,0)
-    
-    
-    A_l_half = A[0:(len(A)/2)]
-    A_r_half = A[(len(A)/2):len(A)]
 
-    B_l_half = B[0:(len(B)/2)]
-    B_r_half = B[(len(B)/2):len(B)]
-    
-    binary_mult(A_r_half, B_r_half)
-    binary_mult(A_l_half, B_l_half)
-    
+    Al = A[0:(len(A)/2)]
+    Ar = A[(len(A)/2):len(A)]
+
+    Bl = B[0:(len(B)/2)]
+    Br = B[(len(B)/2):len(B)]
+
+    a = binary_mult(Al,Bl)
+    b = binary_mult(Al,Br)
+    c = binary_mult(Ar,Bl)
+    d = binary_mult(Ar,Br)
+
+    x = binary_shift(a, n)
+    y = binary_shift(binary_add(b,c), n/2)
+
+    result = (binary_add(binary_add(x,y), d))
+
+    #Make length of result 2*n
+    while len(result)!= 2*n:
+        result.insert(0,0)
+
+    return result
 
 
 class BinaryMultTest(unittest.TestCase):
     """Test Suite for binary multiplication problem
-    
-    Any method named "test_something" will be run when this file is 
-    executed. Use the sanity check as a template for adding your own 
-    tests if you wish. 
+
+    Any method named "test_something" will be run when this file is
+    executed. Use the sanity check as a template for adding your own
+    tests if you wish.
     (You may delete this class from your submitted solution.)
     """
-    
+
     def test_sanity(self):
         """Sanity Test
-        
+
         This is a simple sanity check for your function;
         passing is not a guarantee of correctness.
         """
@@ -57,7 +86,18 @@ class BinaryMultTest(unittest.TestCase):
         B = [0,0,1,0]
         answer = binary_mult(A, B)
         self.assertEqual(answer, [0,0,0,0,1,1,0,0])
+    def test_sanity_uneven(self):
+        """Sanity Test
+
+        This is a simple sanity check for your function;
+        passing is not a guarantee of correctness.
+        """
+        A = [0,1,0]
+        B = [0,1,1]
+        answer = binary_mult(A, B)
+        self.assertEqual(answer, [0,0,0,1,1,0])
+
+
 
 if __name__ == '__main__':
     unittest.main()
-
